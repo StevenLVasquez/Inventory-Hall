fd7455311ecfc8ed8b80c866299b0ff4560ff00c
Steven Vasquez <stevenvasquez652@gmail.com>
2023-10-25 20:26:12 -0400

    agregue la funcion de que solo se pueda abrir una ventana a la vez

--- Inventory Hall/principal.cs
+++ Inventory Hall/principal.cs
@@ -20,47 +20,49 @@ namespace Inventory_Hall
         //calling form ( agr_producto)
         private void pRODUCTOToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            agrproducto agrproducto = new agrproducto();
-            agrproducto.Show();
-
-
+            using (agrproducto agrproducto = new agrproducto())
+            {
+                agrproducto.ShowDialog(); // Open the form as a modal dialog
+            }
         }
         //calling form (agr_empleado)
         private void eMPLEADOToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            agrempleado agrempleado = new agrempleado();
-            agrempleado.Show();
+            using (agrempleado agrempleado = new agrempleado())
+            {
+                agrempleado.ShowDialog();// here im using ( using ) bc its the right way 
+            }
         }
         //calling form ( agr_suplidor )
         private void sUPLIDORToolStripMenuItem_Click(object sender, EventArgs e)
         {
             agrsuplidor agrsuplidor = new agrsuplidor();
-            agrsuplidor.Show();
+            agrsuplidor.ShowDialog(); // here im just using showdialog to see if it works , later ill change it if it gives me any problems with the code 
         }
         //calling form ( pro_distribucion de articulos)
         private void dISTRIBUCIONDEARTICULOSToolStripMenuItem_Click(object sender, EventArgs e)
         {
             prodistribucion prodistribucion = new prodistribucion();
-            prodistribucion.Show();
+            prodistribucion.ShowDialog();
         }
         // calling form ( con_producto )
         private void pRODUCTOToolStripMenuItem1_Click(object sender, EventArgs e)
         {
             conproducto conproducto = new conproducto();
-            conproducto.Show();
+            conproducto.ShowDialog();
 
         }
         //calling form ( con_empleado )
         private void eMPLEADOToolStripMenuItem1_Click(object sender, EventArgs e)
         {
             conempleado conempleado = new conempleado();
-            conempleado.Show();
+            conempleado.ShowDialog();
         }
         //calling form ( con_suplidor )
         private void sUPLIDORToolStripMenuItem1_Click(object sender, EventArgs e)
         {
             consuplidor consuplidor = new consuplidor();   
-            consuplidor.Show();
+            consuplidor.ShowDialog();
         }
     }
 }
